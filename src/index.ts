1473fb81220e3480e26de9ceeaa6f2e19a442d12
Robert Całka <calka.rob@gmail.com>
2018-08-12 12:15:24 +0200

    change way to render parts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ window.addEventListener("load", function(event) {
     mainDiv.style.background = "#0ff";
     mainDiv.style.marginBottom = "20px"; 
     mainDiv.style.cssFloat = "left"
-    mainDiv.style.background = `linear-gradient(180deg, #fff, #FF1122, #000, #FF9955, #fff)`
+    mainDiv.style.background = "transparent"
+    // mainDiv.style.background = `linear-gradient(180deg, #fff, #FF1122, #000, #FF9955, #fff)`
     body.appendChild(mainDiv);
 
     let wrapper:HTMLElement = document.createElement('div');
@@ -26,8 +27,8 @@ window.addEventListener("load", function(event) {
     const _colors = [0xFFFFFF, 0xFF1122, 0x000000, 0xFF9955, 0xFFFFFF]
     const level_1 = new Level({
         elem: wrapper,
-        rows: 2,
-        cols: 1,
+        rows: 5,
+        cols: 3,
         gradient: { 
             type: GradientType.LINEAR, 
             
@@ -88,7 +89,8 @@ class Level {
     rows:number;
     cols:number;
     childrens: Array<HTMLElement> = [];
-    gradient: IGradient
+    gradient: IGradient;
+    mainChildCanvas: HTMLCanvasElement;
 
     constructor(initOptions: ILevelInput){
         // wyzerowac kontener z poprzednich danych
@@ -100,186 +102,81 @@ class Level {
         // stworzyc dzieci 
         this.rows = initOptions.rows;
         this.cols = initOptions.cols;
+        this.createMainChildCanvas();
         this.createChildrens();
 
         // nalozyc gradient na dzieci
-        this.gradient = initOptions.gradient;
-        this.fillChildrendWithGradient();
+        // this.gradient = initOptions.gradient;
+        // this.fillChildrendWithGradient();
         // zablokowac dzieci ktore sa w .blocked
         // podpiac eventy do dziecki 
         // nasluchiwac na event zakoczenia levela
     }
 
-// DODAC gradient na canvas
-// var  gr  = context.createLinearGradient(0,  0,  100,  0);
-// // Add the   color stops. 
-// gr.addColorStop(0,'rgb(255,0,0)'); 
-// gr.addColorStop(.5,'rgb(0,255,0)'); 
-// gr.addColorStop(1,'rgb(255,0,0)');
-// // Use the gradient
-// fillStyle.context.fillStyle  = gr; 
-// context.fillRect(0,  0,100,100);
 
-// Dodaj clipping do elementu
-// Create a shape, of some sort
-// Save the state, so we can undo the clipping
-// ctx.save();
-// ctx.beginPath();
-// ctx.moveTo(10, 10);
-// ctx.lineTo(100, 30);
-// ctx.lineTo(180, 10);
-// ctx.lineTo(200, 60);
-// ctx.arcTo(180, 70, 120, 0, 10);
-// ctx.lineTo(200, 180);
-// ctx.lineTo(100, 150);
-// ctx.lineTo(70, 180);
-// ctx.lineTo(20, 130);
-// ctx.lineTo(50, 70);
-// ctx.closePath();
-// // Clip to the current path
-// ctx.clip();
-// ctx.drawImage(img, 0, 0);
-// Undo the clipping
-// ctx.restore();
+    createMainChildCanvas(){
+        let mainChildCanvas = document.createElement('canvas');
+        mainChildCanvas.width = this.parentWidth;
+        mainChildCanvas.height = this.parentHeight;
+        mainChildCanvas.style.opacity = "1";
+        mainChildCanvas.style.position = "absolute";
+        mainChildCanvas.style.top = "0px";
+        mainChildCanvas.style.left = "-501px";
+        mainChildCanvas.style.zIndex = "0";
 
-
-
-    fillChildrendWithGradient(){
-        // interface IGradient {
-        //     type: GradientType,
-        //     colors: Array<number>,
-        //     angleDeg: number 
-        // }
+        let mainChildCanvasCTX = mainChildCanvas.getContext('2d');
         
-        const hexToRgb = (hex:number) => {
-            var r = (hex >> 16) & 255;
-            var g = (hex >> 8) & 255;
-            var b = hex & 255;
-            return r + "," + g + "," + b;
-        }
+        // get angle from bottom left to top right
+        let pheta = Math.atan2(-mainChildCanvas.height, mainChildCanvas.width);
 
-        const calculateRgbByPercentage = (colorStart:number, colorEnd:number, percentage:number) =>{
-            let r_start = (colorStart >> 16) & 255;
-            let g_start = (colorStart >> 8) & 255;
-            let b_start = colorStart & 255;
+        // get the length of the line from bottom right to diagonal line we got the ang of.
+        // dont need the abd but cant be bothered explaining why its negative 
+        let AB = Math.abs(mainChildCanvas.height * Math.cos(pheta)); 
+        
+        // get vector at 90 deg from found angle
+        let xdx = Math.cos(pheta + Math.PI/2);
+        let xdy = Math.sin(pheta + Math.PI/2);
 
-            let r_end = (colorEnd >> 16) & 255;
-            let g_end = (colorEnd >> 8) & 255;
-            let b_end = colorEnd & 255;
-
-            
-            let r_finish = r_start + ((r_end - r_start) * percentage);
-            let g_finish = g_start + ((g_end - g_start) * percentage);
-            let b_finish = b_start + ((b_end - b_start) * percentage);
-            console.log('COLOR R', r_start, r_end, r_finish) 
-            console.log('COLOR G', g_start, g_end, g_finish) 
-            console.log('COLOR B', b_start, b_end, b_finish)
-            console.log('COLOR %', percentage)
-
-            return `rgb(${r_finish},${g_finish},${b_finish})`;
-        }
-
-        const calculateCssColor = (y:number) => {
-            let containerHeight:number = this.parentHeight
-            let colorHeight:number = containerHeight / (this.gradient.colors.length - 1);
-
-            let gradientColorIndexStart:number = Math.floor(y / colorHeight); 
-            let gradientColorIndexEnd:number = gradientColorIndexStart + 1;
-           
-            if(gradientColorIndexEnd >= this.gradient.colors.length){
-                gradientColorIndexEnd = gradientColorIndexStart; 
-                gradientColorIndexStart = gradientColorIndexStart - 1; 
-            }
-
-            let gradientColorStart = this.gradient.colors[gradientColorIndexStart];
-            let gradientColorEnd = this.gradient.colors[gradientColorIndexEnd];
-
-            let colorStartHeight = gradientColorIndexStart * colorHeight;
-            let percentageColorStart = (y - colorStartHeight) / colorHeight;
-            let diffHeightInPx =  colorHeight - (y - colorStartHeight);  
+        // from the center C of rectangle move AB dist back and forward to find points E and D
+        let x1 = mainChildCanvas.width/2 - xdx * AB;
+        let y1 = mainChildCanvas.height/2 - xdy * AB;
+        let x2 = mainChildCanvas.width/2 + xdx * AB;
+        let y2 = mainChildCanvas.height/2 + xdy * AB;
 
 
-            console.log('KOLORY', hexToRgb(gradientColorStart), hexToRgb(gradientColorEnd))
-            console.log("%%", percentageColorStart, y, colorStartHeight, colorHeight, gradientColorIndexStart);
-            
-            let cssColorFinish = calculateRgbByPercentage(gradientColorStart, gradientColorEnd, percentageColorStart);
-            
-            return {
-                css: cssColorFinish,
-                gradientColorIndexStart: gradientColorIndexStart,
-                gradientColorIndexEnd: gradientColorIndexEnd,
-                diffHeightInPx: diffHeightInPx,
-            }
-        }
+        // let length = mainChildCanvas.height + 150;
+        // let angle = 45;
+        // let x1 = 0;
+        // let y1 = 0;
+        // let x2 = length * Math.cos(angle);  // angle in radians
+        // let y2 = length * Math.sin(angle);  // angle in radians
+        // mainChildCanvas.height
+        let gradient = mainChildCanvasCTX.createLinearGradient(x1, y1, x2, y2);
+        gradient.addColorStop(0, '#fff');
+        gradient.addColorStop(0.5, '#0ff');
+        gradient.addColorStop(0.9, '#bada55');
+        gradient.addColorStop(1, '#000000');  
+        mainChildCanvasCTX.fillStyle = gradient;
+        mainChildCanvasCTX.fillRect(0, 0, mainChildCanvas.width, mainChildCanvas.height);
 
-        if(this.gradient.type == GradientType.LINEAR){
-            let cssGradientType = "linear-gradient";
-            let cssAngleDeg = `${this.gradient.angleDeg}deg`;
-           
-            for(let i:number = 0, child; child = this.childrens[i]; i++){
-                let { top, left, width, height } = child.style;
-                let x1:number = parseInt(left, 10);
-                let y1:number = parseInt(top, 10);
-                let x2:number = parseInt(left, 10) + parseInt(width, 10);
-                let y2:number = parseInt(top, 10) + parseInt(height, 10);
-
-                console.log('---WIERSZ', i)
-                
-                let colStart = calculateCssColor(y1);
-                let colEnd = calculateCssColor(y2);
-
-                console.log('-- ',i,  colStart, colEnd);
-
-                // let indexAmount = colEnd.gradientColorIndexEnd - colStart.gradientColorIndexStart;
-                // let containerHeight:number = this.parentHeight
-                // let colorHeight:number = containerHeight / (this.gradient.colors.length - 1);
-                // let aa = colorHeight / containerHeight;
-
-                
-                let percentageStart = 0;
-                if(i == 0){
-                    // percentageStart = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
-                    
-                }else{
-                    // percentageStart = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
-                }
-                percentageStart = colStart.diffHeightInPx / parseInt(height, 10);  
-
-                let percentageCss = (percentageStart) ? percentageStart + "%": ""
-                let cssColors = `${colStart.css} ${percentageCss}, `;
-                for(let j = colStart.gradientColorIndexEnd; j < colEnd.gradientColorIndexEnd; j++){
-                    // let percentageCurrent:number = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
-                    // console.log('------------- co mamy w przesunieciu?', percentageCurrent, colStart.diffHeightInPx, parseInt(height, 10));
-                    // Wys = wysokosc przesuniecia wzgledem poczatku kolor w px
-                    // ProcentPrzesuniecia = cala wysokosc bloku pojedycznego / Wys
-                    let currentColor = this.gradient.colors[j];
-                    cssColors += `rgb(${hexToRgb(currentColor)}), `
-                }
-                cssColors += `${colEnd.css}`;
-                console.log(' ?? ', i, cssColors);
-                let cssBackground = `${cssGradientType}(${cssAngleDeg}, ${cssColors})`
-                console.log('co wyszlo?', cssBackground, cssAngleDeg)                
-                
-                child.style.background = cssBackground;   
-                child.style.border = "1px solid #0FF"; 
-                child.style.boxSizing = "border-box";
-            }
-        }
+        this.parentElement.appendChild(mainChildCanvas);
+        this.mainChildCanvas = mainChildCanvas;
     }
 
     createChildrens(){
         let iterateCount = this.rows * this.cols;
-        let childHeight = this.parentHeight / this.rows;
-        let childWidth = this.parentWidth / this.cols;
+        let childHeight = Math.ceil(this.parentHeight / this.rows);
+        let childWidth = Math.ceil(this.parentWidth / this.cols);
 
         for(let i:number = 0; i < iterateCount; i++){
             let child = document.createElement('div');
 
-            child.style.background = "#0ff";
+            child.style.background = "transparent";
             child.style.width = `${childWidth}px`;
             child.style.height = `${childHeight}px`;
             child.style.position = "absolute";
-            
+            child.style.zIndex = "0";
+
             let currentRow = Math.floor(i / this.cols);
             let top = childHeight * currentRow;
             child.style.top = `${top}px`;
@@ -287,11 +184,170 @@ class Level {
             let curretCol = Math.floor(i % this.cols);
             let left = childWidth * curretCol;
             child.style.left = `${left}px`;
-            
+
+            let childCanvas = document.createElement('canvas');
+            childCanvas.width = childWidth;
+            childCanvas.height = childHeight;
+            let childCTX = childCanvas.getContext('2d');
+
+            childCTX.drawImage(this.mainChildCanvas, 
+                left, top, childWidth, childHeight, 0, 0, childWidth, childHeight);
+
+            child.appendChild(childCanvas);
             this.parentElement.appendChild(child);
             this.childrens.push(child);
         }
     }
+
+
+    // fillChildrendWithGradient(){
+    //     // interface IGradient {
+    //     //     type: GradientType,
+    //     //     colors: Array<number>,
+    //     //     angleDeg: number 
+    //     // }
+        
+    //     const hexToRgb = (hex:number) => {
+    //         var r = (hex >> 16) & 255;
+    //         var g = (hex >> 8) & 255;
+    //         var b = hex & 255;
+    //         return r + "," + g + "," + b;
+    //     }
+
+    //     const calculateRgbByPercentage = (colorStart:number, colorEnd:number, percentage:number) =>{
+    //         let r_start = (colorStart >> 16) & 255;
+    //         let g_start = (colorStart >> 8) & 255;
+    //         let b_start = colorStart & 255;
+
+    //         let r_end = (colorEnd >> 16) & 255;
+    //         let g_end = (colorEnd >> 8) & 255;
+    //         let b_end = colorEnd & 255;
+
+            
+    //         let r_finish = r_start + ((r_end - r_start) * percentage);
+    //         let g_finish = g_start + ((g_end - g_start) * percentage);
+    //         let b_finish = b_start + ((b_end - b_start) * percentage);
+    //         console.log('COLOR R', r_start, r_end, r_finish) 
+    //         console.log('COLOR G', g_start, g_end, g_finish) 
+    //         console.log('COLOR B', b_start, b_end, b_finish)
+    //         console.log('COLOR %', percentage)
+
+    //         return `rgb(${r_finish},${g_finish},${b_finish})`;
+    //     }
+
+    //     const calculateCssColor = (y:number) => {
+    //         let containerHeight:number = this.parentHeight
+    //         let colorHeight:number = containerHeight / (this.gradient.colors.length - 1);
+
+    //         let gradientColorIndexStart:number = Math.floor(y / colorHeight); 
+    //         let gradientColorIndexEnd:number = gradientColorIndexStart + 1;
+           
+    //         if(gradientColorIndexEnd >= this.gradient.colors.length){
+    //             gradientColorIndexEnd = gradientColorIndexStart; 
+    //             gradientColorIndexStart = gradientColorIndexStart - 1; 
+    //         }
+
+    //         let gradientColorStart = this.gradient.colors[gradientColorIndexStart];
+    //         let gradientColorEnd = this.gradient.colors[gradientColorIndexEnd];
+
+    //         let colorStartHeight = gradientColorIndexStart * colorHeight;
+    //         let percentageColorStart = (y - colorStartHeight) / colorHeight;
+    //         let diffHeightInPx =  colorHeight - (y - colorStartHeight);  
+
+
+    //         console.log('KOLORY', hexToRgb(gradientColorStart), hexToRgb(gradientColorEnd))
+    //         console.log("%%", percentageColorStart, y, colorStartHeight, colorHeight, gradientColorIndexStart);
+            
+    //         let cssColorFinish = calculateRgbByPercentage(gradientColorStart, gradientColorEnd, percentageColorStart);
+            
+    //         return {
+    //             css: cssColorFinish,
+    //             gradientColorIndexStart: gradientColorIndexStart,
+    //             gradientColorIndexEnd: gradientColorIndexEnd,
+    //             diffHeightInPx: diffHeightInPx,
+    //         }
+    //     }
+
+    //     if(this.gradient.type == GradientType.LINEAR){
+    //         let cssGradientType = "linear-gradient";
+    //         let cssAngleDeg = `${this.gradient.angleDeg}deg`;
+           
+    //         for(let i:number = 0, child; child = this.childrens[i]; i++){
+    //             let { top, left, width, height } = child.style;
+    //             let x1:number = parseInt(left, 10);
+    //             let y1:number = parseInt(top, 10);
+    //             let x2:number = parseInt(left, 10) + parseInt(width, 10);
+    //             let y2:number = parseInt(top, 10) + parseInt(height, 10);
+
+    //             console.log('---WIERSZ', i)
+                
+    //             let colStart = calculateCssColor(y1);
+    //             let colEnd = calculateCssColor(y2);
+
+    //             console.log('-- ',i,  colStart, colEnd);
+
+    //             // let indexAmount = colEnd.gradientColorIndexEnd - colStart.gradientColorIndexStart;
+    //             // let containerHeight:number = this.parentHeight
+    //             // let colorHeight:number = containerHeight / (this.gradient.colors.length - 1);
+    //             // let aa = colorHeight / containerHeight;
+
+                
+    //             let percentageStart = 0;
+    //             if(i == 0){
+    //                 // percentageStart = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
+                    
+    //             }else{
+    //                 // percentageStart = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
+    //             }
+    //             percentageStart = colStart.diffHeightInPx / parseInt(height, 10);  
+
+    //             let percentageCss = (percentageStart) ? percentageStart + "%": ""
+    //             let cssColors = `${colStart.css} ${percentageCss}, `;
+    //             for(let j = colStart.gradientColorIndexEnd; j < colEnd.gradientColorIndexEnd; j++){
+    //                 // let percentageCurrent:number = colStart.diffHeightInPx / parseInt(height, 10) - 100;  
+    //                 // console.log('------------- co mamy w przesunieciu?', percentageCurrent, colStart.diffHeightInPx, parseInt(height, 10));
+    //                 // Wys = wysokosc przesuniecia wzgledem poczatku kolor w px
+    //                 // ProcentPrzesuniecia = cala wysokosc bloku pojedycznego / Wys
+    //                 let currentColor = this.gradient.colors[j];
+    //                 cssColors += `rgb(${hexToRgb(currentColor)}), `
+    //             }
+    //             cssColors += `${colEnd.css}`;
+    //             console.log(' ?? ', i, cssColors);
+    //             let cssBackground = `${cssGradientType}(${cssAngleDeg}, ${cssColors})`
+    //             console.log('co wyszlo?', cssBackground, cssAngleDeg)                
+                
+    //             child.style.background = cssBackground;   
+    //             child.style.border = "1px solid #0FF"; 
+    //             child.style.boxSizing = "border-box";
+    //         }
+    //     }
+    // }
+
+    // createChildrens(){
+    //     let iterateCount = this.rows * this.cols;
+    //     let childHeight = this.parentHeight / this.rows;
+    //     let childWidth = this.parentWidth / this.cols;
+
+    //     for(let i:number = 0; i < iterateCount; i++){
+    //         let child = document.createElement('div');
+
+    //         child.style.background = "#0ff";
+    //         child.style.width = `${childWidth}px`;
+    //         child.style.height = `${childHeight}px`;
+    //         child.style.position = "absolute";
+            
+    //         let currentRow = Math.floor(i / this.cols);
+    //         let top = childHeight * currentRow;
+    //         child.style.top = `${top}px`;
+
+    //         let curretCol = Math.floor(i % this.cols);
+    //         let left = childWidth * curretCol;
+    //         child.style.left = `${left}px`;
+            
+    //         this.parentElement.appendChild(child);
+    //         this.childrens.push(child);
+    //     }
+    // }
 
     readContainerSize(){
         this.parentHeight = this.parentElement.clientHeight;
